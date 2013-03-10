7916f3bf40344f67b846328cb45afaebef9647aa
Raghu Betina <rbetina@gmail.com>
2013-03-10 14:40:35 -0500

    rails new, disabled SSL verification.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Friendbc::Application.config.secret_token = 'f5b46b5c3587c923eb2d2efcac5a1a5bf5b3b63e296cbd0316952471b1b8cfe65d4ffea7d937da83bf8626cfee6b5f4e96539e05546f765d0beed18931a6c671'
